8b5732e67b73f365071eaf60647fa78e760bd81c
qasimimran1 <qasimimran1@github.com>
2020-11-26 20:12:15 +0200

    No of input files variable

--- main.c
+++ main.c
@@ -4,37 +4,35 @@
 #define NO_OF_FILES 10
 #define FILE_NAME_LEN 20
 
+char **create_file_matrix(int nrows, int ncols)
+{
+    // first of all allocate space for the rows
+    // each entry is the size of an char pointer
+    char **m = malloc(nrows * sizeof(char *));
 
-char** create_file_matrix(int nrows, int ncols) {
-	// first of all allocate space for the rows
-	// each entry is the size of an char pointer
-	char **m = malloc(nrows*sizeof(char*));
-    
-	// for each row, allocate space for the columns
-	// each entry is the size of an char
-	for(int i=0; i<nrows; i++) {
-		m[i] = malloc(ncols*sizeof(char));        
-	}
-    
-	return m;
+    // for each row, allocate space for the columns
+    // each entry is the size of an char
+    for (int i = 0; i < nrows; i++)
+    {
+        m[i] = malloc(ncols * sizeof(char));
+    }
+
+    return m;
 }
-
-
-
 
 int main(int argc, char **argv)
 {
-    
+
     // Set default settings
-    char **input_files    = create_file_matrix(NO_OF_FILES, FILE_NAME_LEN);
-    dumper_setting_t dumper_opts; 
-    dumper_opts.fout_name   = NULL;
-    dumper_opts.input       = STD_IO; 
-    dumper_opts.output      = STD_IO;
-    dumper_opts.col_size    = 16;
 
+    dumper_setting_t dumper_opts;
+    dumper_opts.fin_name = NULL;
+    dumper_opts.fout_name = NULL;
+    dumper_opts.input = STD_IO;
+    dumper_opts.output = STD_IO;
+    dumper_opts.col_size = 16;
 
-    uint8_t save = 0, f_indx = 0;
+    uint8_t save = 0, f_indx = 0, start_indx = 0;
 
     for (size_t i = 0; i < argc; i++)
     {
@@ -42,50 +40,48 @@ int main(int argc, char **argv)
         {
             save = 1;
             i++;
+            start_indx = i;
         }
         if (save)
         {
-            dumper_opts.input  = FILE_IO;
-            input_files[f_indx] = argv[i];
-            //
             f_indx++;
         }
     }
 
-    process_options(argc,argv, &dumper_opts);
-
-    // if (dumper_opts.fin_name != NULL)
-    // {
-        
-    //     for (size_t i = 0; i < sizeof(dumper_opts.fin_name); i++)
-    //     {
-    //         printf("fin_name %s: \n",  dumper_opts.fin_name[i]);
-    //     }       
-
-    // }
-    if (dumper_opts.fout_name != NULL)
-    {
-        dumper_opts.output  = FILE_IO;
-        printf("fout_name %s: \n", dumper_opts.fout_name);
-    }
-
+    char **input_files = create_file_matrix(f_indx, FILE_NAME_LEN);
     for (size_t i = 0; i < f_indx; i++)
     {
-      dumper_opts.fin_name =  input_files[i];
-      printf("inputfile name: %s\n", dumper_opts.fin_name);
-      dump_file(&dumper_opts);
+        input_files[i] = argv[start_indx + i];
     }
-    
 
- //   dump_file(&dumper_opts);
+    process_options(argc, argv, &dumper_opts);
+
+    if (dumper_opts.fout_name != NULL)
+    {
+        dumper_opts.output = FILE_IO;
+        printf("fout_name %s: \n", dumper_opts.fout_name);
+    }
+    if (f_indx > 0)
+    {
+        dumper_opts.input = FILE_IO; 
+        for (size_t i = 0; i < f_indx; i++)
+        {
+            dumper_opts.fin_name = input_files[i];
+            printf("inputfile name: %s\n", dumper_opts.fin_name);
+            dump_file(&dumper_opts);
+        }
+    }
+    else
+    {
+        dump_file(&dumper_opts);
+    }
+
+    //   dump_file(&dumper_opts);
 
     //  for(int i=0; i<NO_OF_FILES; i++) {
-	// 	free(input_files[i]);
-	// }
-	free(input_files);
-
- 
-    
+    // 	free(input_files[i]);
+    // }
+    free(input_files);
 
     return 0;
 }
